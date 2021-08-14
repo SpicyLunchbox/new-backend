4ae0f38c25b489b7500d8f30846795f4fd51478f
SpicyLunchbox <westonwoodard28@gmail.com>
2021-08-13 19:14:10 -0700

    migrations fix

--- api/data/seeds/001-users.js
+++ api/data/seeds/001-users.js
@@ -1,12 +1,11 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
-    return knex('users').truncate()
-      .then(function () {
+    // return knex('users').truncate()
+    //   .then(function () {
         // Inserts seed entries
         return knex('users').insert([
           {username: 'user1', password: '$2a$08$yxq4ZrmvRRDqktMadY54LOEIbmioEgBmPRcXx0/.xD0RGmLwc3S4G'},
           {username: 'user2', password: '$2a$08$yxq4ZrmvRRDqktMadY54LOEIbmioEgBmPRcXx0/.xD0RGmLwc3S4G'},
           {username: 'user3', password: '$2a$08$yxq4ZrmvRRDqktMadY54LOEIbmioEgBmPRcXx0/.xD0RGmLwc3S4G'}
         ]);
-      });
-  };
+      };
--- api/data/seeds/002-equipment.js
+++ api/data/seeds/002-equipment.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
-    return knex('equipment').truncate()
-      .then(function () {
+    // return knex('equipment').truncate()
+    //   .then(function () {
         // Inserts seed entries
         return knex('equipment').insert([
           {equipment_name: 'camera1', equipment_description: 'this is a test description for equipment 1', owner_id: 2},
@@ -10,6 +10,4 @@ exports.seed = function(knex) {
           {equipment_name: 'camera4', equipment_description: 'this is a test description for equipment 4', owner_id: 3},
           {equipment_name: 'VCR5', equipment_description: 'this is a test description for equipment 5', owner_id: 3}
         ]);
-      });
-  };
-  
+      };
